733e4befef1f17fbeec89f44609fdd7ac3c35fa4
Zichen Wang <wangzc921@gmail.com>
2016-11-25 11:51:36 -0500

    Transfered drug table to euclid db

--- scripts/truncate_all_tables.sh
+++ scripts/truncate_all_tables.sh
@@ -4,3 +4,7 @@ DATABASE_NAME='euclid'
 mysql -u root -Nse 'show tables' $DATABASE_NAME | while read table; do mysql -u root -e "SET FOREIGN_KEY_CHECKS = 0; drop table $table" $DATABASE_NAME; done
 ## Restore the schema 
 mysql -u root $DATABASE_NAME < euclid.schema.sql
+## Transfer the `drugs_lincs` table and rename it
+mysqldump -u root maaya0_SEP drugs_lincs \
+| sed -e 's/`drugs_lincs`/`drug`/' \
+| mysql -u root $DATABASE_NAME
